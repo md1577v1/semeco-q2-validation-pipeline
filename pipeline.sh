96683d349c09fb8b8d4e0bbbaee2707f34ba4690
Martin Diller <martin.diller@tu-dresden.de>
2026-03-08 14:48:40 +0100

    same for pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -16,6 +16,7 @@ prob=5
 sev=5
 
 mode="html" # html or abox, default html
+enrichment="plain"   # plain, riskman, secuman - default plain
 
 # ./pipeline.sh -h /home/piotr/Dresden/kimeds/riskman-reasoning-pipeline/riskman-validation-pipeline/test-cases/submission_giip.html -o /home/piotr/Dresden/kimeds/riskman-reasoning-pipeline/riskman-validation-pipeline/ontology.ttl -c /home/piotr/Dresden/kimeds/riskman-reasoning-pipeline/riskman-validation-pipeline/shapes.ttl -p 5 -s 5 -m html
 
@@ -27,7 +28,7 @@ mode="html" # html or abox, default html
 
 
 
-while getopts "h:a:o:c:p:s:m:" opt; do
+while getopts "h:a:o:c:p:s:m:e:" opt; do
     case "$opt" in
         h) html="$OPTARG" ;;
         a) abox="$OPTARG" ;;
@@ -36,7 +37,11 @@ while getopts "h:a:o:c:p:s:m:" opt; do
         p) prob="$OPTARG" ;;
         s) sev="$OPTARG" ;;
         m) mode="$OPTARG" ;;
-        # *) usage ;;
+        e) enrichment="$OPTARG" ;;
+        *)
+            echo "Usage: $0 [-h html] [-a abox] -o ontology -c shapes -m [html|abox] -e [plain|riskman|secuman] [-p prob] [-s sev]" >&2
+            exit 1
+            ;;
     esac
 done
 
@@ -44,25 +49,56 @@ done
 # option 1 
 # HTML file with RDF encoding
 modeHtml() {
-    docker run \
-        -v $(realpath "$html"):$containerPath/$htmlTarget \
-        -v $(realpath "$ontology"):$containerPath/$ontologyTarget \
-        -v $(realpath "$shapes"):$containerPath/$shapesTarget \
-        -t $containerName -p $prob -s $sev -m html
+    if [ -z "$html" ] || [ -z "$ontology" ] || [ -z "$shapes" ]; then
+        echo "HTML mode requires -h, -o, and -c" >&2
+        exit 1
+    fi
+
+    if [ "$enrichment" = "riskman" ]; then
+        docker run \
+            -v "$(realpath "$html")":"$containerPath/$htmlTarget" \
+            -v "$(realpath "$ontology")":"$containerPath/$ontologyTarget" \
+            -v "$(realpath "$shapes")":"$containerPath/$shapesTarget" \
+            -t "$containerName" -p "$prob" -s "$sev" -m html -e "$enrichment"
+    else
+        docker run \
+            -v "$(realpath "$html")":"$containerPath/$htmlTarget" \
+            -v "$(realpath "$ontology")":"$containerPath/$ontologyTarget" \
+            -v "$(realpath "$shapes")":"$containerPath/$shapesTarget" \
+            -t "$containerName" -m html -e "$enrichment"
+    fi
 }
 
 # option 2
 # directly providing ABox 
 modeAbox() {
-    docker run \
-        -v $(realpath "$abox"):$containerPath/$aboxTarget \
-        -v $(realpath "$ontology"):$containerPath/$ontologyTarget \
-        -v $(realpath "$shapes"):$containerPath/$shapesTarget \
-        -t $containerName -p $prob -s $sev -m abox
+     if [ -z "$abox" ] || [ -z "$ontology" ] || [ -z "$shapes" ]; then
+        echo "ABox mode requires -a, -o, and -c" >&2
+        exit 1
+    fi
+
+    if [ "$enrichment" = "riskman" ]; then
+        docker run \
+            -v "$(realpath "$abox")":"$containerPath/$aboxTarget" \
+            -v "$(realpath "$ontology")":"$containerPath/$ontologyTarget" \
+            -v "$(realpath "$shapes")":"$containerPath/$shapesTarget" \
+            -t "$containerName" -p "$prob" -s "$sev" -m abox -e "$enrichment"
+    else
+        docker run \
+            -v "$(realpath "$abox")":"$containerPath/$aboxTarget" \
+            -v "$(realpath "$ontology")":"$containerPath/$ontologyTarget" \
+            -v "$(realpath "$shapes")":"$containerPath/$shapesTarget" \
+            -t "$containerName" -m abox -e "$enrichment"
+    fi
 }
 
 if [ "$mode" == "html" ]; then
     modeHtml
 elif [ "$mode" == "abox" ]; then
     modeAbox
+fi
+else
+    echo "Unknown mode: $mode" >&2
+    echo "Usage: $0 [-h html] [-a abox] -o ontology -c shapes -m [html|abox] -e [plain|riskman|secuman] [-p prob] [-s sev]" >&2
+    exit 1
 fi
