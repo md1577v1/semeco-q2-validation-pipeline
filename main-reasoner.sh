cbdd533346a9f5a07c0accbbbb923ec575aebbc7
Martin Diller <martin.diller@tu-dresden.de>
2026-03-16 16:12:56 +0100

    make enrichement option generic in main-reasoner.sh as well

--- main-reasoner.sh
+++ main-reasoner.sh
@@ -4,18 +4,26 @@
 html="input/submission.html"
 abox="input/abox.ttl"
 ontology="input/ontology.ttl"
-#
-prob=5
-sev=5
 
 mode="html" # html or abox, default html
+enrichment="plain" # plain, riskman, secuman, default plain
+
+# RiskMan parameters
+prob=""
+sev=""
 
 
-while getopts "p:s:m:" opt; do
+
+while getopts "m:e:p:s:" opt; do
     case "$opt" in
+        m) mode="$OPTARG" ;;
+        e) enrichment="$OPTARG" ;;
         p) prob="$OPTARG" ;;
         s) sev="$OPTARG" ;;
-        m) mode="$OPTARG" ;;
+        *)
+            echo "Usage: $0 -m [html|abox] -e [plain|riskman|secuman] [-p prob] [-s sev]" >&2
+            exit 1
+            ;;
     esac
 done
 
@@ -24,23 +32,53 @@ done
 # Shift off the options and optional --
 shift $((OPTIND-1))
 
+run_enrichment() {
+    case "$enrichment" in
+        plain)
+            cat
+            ;;
+        riskman)
+            if [ -z "$prob" ] || [ -z "$sev" ]; then
+                echo "RiskMan enrichment requires -p and -s" >&2
+                exit 1
+            fi
+            ./prob_sev.sh -p "$prob" -s "$sev"
+            ;;
+        secuman)
+            #./secuman_enrichment.sh 
+            #Stub for now
+            cat
+            ;;
+        *)
+            echo "Unknown enrichment: $enrichment" >&2
+            echo "Usage: $0 -m [html|abox] -e [plain|riskman|secuman] [-p prob] [-s sev]" >&2
+            exit 1
+            ;;
+    esac
+}
+
+
 
 # option 1
 # HTML file with RDF encoding
 # STOPS AFTER REASONING - outputs inferred RDF
 modeHtml() {
-    cat $html | ./rdf_distiller.sh | cat - $ontology | ./prob_sev.sh -p $prob -s $sev | ./reasoner.sh
+    cat $html | ./rdf_distiller.sh | cat - $ontology | run_enrichment | ./reasoner.sh
 }
 
 # option 2
 # directly providing ABox
 # STOPS AFTER REASONING - outputs inferred RDF
 modeAbox() {
-    cat $abox $ontology | ./prob_sev.sh -p $prob -s $sev | ./reasoner.sh
+    cat $abox $ontology | run_enrichment | ./reasoner.sh
 }
 
 if [ "$mode" == "html" ]; then
     modeHtml
 elif [ "$mode" == "abox" ]; then
     modeAbox
+else
+    echo "Unknown mode: $mode" >&2
+    echo "Usage: $0 -m [html|abox] -e [plain|riskman|secuman] [-p prob] [-s sev]" >&2
+    exit 1
 fi
